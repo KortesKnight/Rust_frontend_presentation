d834c31f2a3f3ff9fee668ceae4fb879f0ec3a26
KortesKnight <alekszelik@gmail.com>
2024-09-16 00:32:23 +0400

    Форматирование файла

--- yew-app/src/widget/header.rs
+++ yew-app/src/widget/header.rs
@@ -6,10 +6,10 @@ use yew_router::prelude::*;
 pub fn Header() -> Html {
     html! {
 
-        <div class="fixed w-full flex space-x-4 top-0 left-0 bg-neutral-500">
-                <Link<Route> classes={"p-2 text-white hover:bg-pink-600 hover:text-cyan-400"} to={Route::Main}>{"Main page!"}</Link<Route>>
-                <Link<Route> classes={"p-2 text-white hover:bg-pink-600 hover:text-cyan-400"} to={Route::Fibonacci}>{"Fibonacci page!"}</Link<Route>>
-                <Link<Route> classes={"p-2 text-white hover:bg-pink-600 hover:text-cyan-400"} to={Route::NotFound}>{"Not Found page!"}</Link<Route>>
-            </div>
+        <div class="fixed w-full flex space-x-4 top-0 left-0 bg-neutral-500" style="background-color: white;">
+            <Link<Route> classes={"p-2 text-white hover:bg-pink-600 hover:text-cyan-400"} to={Route::Main}>{"Main page!"}</Link<Route>>
+            <Link<Route> classes={"p-2 text-white hover:bg-pink-600 hover:text-cyan-400"} to={Route::Fibonacci}>{"Fibonacci page!"}</Link<Route>>
+            <Link<Route> classes={"p-2 text-white hover:bg-pink-600 hover:text-cyan-400"} to={Route::NotFound}>{"Not Found page!"}</Link<Route>>
+        </div>
     }
 }
